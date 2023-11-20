3191e7ee1a5852e25c1e588ca06c55ab87effcc2
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-11-20 19:39:35 +0100

    1.2

--- QuickSort_InCSharp/Program.cs
+++ QuickSort_InCSharp/Program.cs
@@ -1,14 +1,21 @@
-﻿namespace QuickSort_InCSharp
+﻿using System.Net.NetworkInformation;
+
+namespace QuickSort_InCSharp
 {
     internal class Program
     {
+        public static Random rn;
+        public static int opc;
+
         static void Main(string[] args)
         {
-            int[] arreglo = { -2, -1, 45, 23, 7, 2, 4, 1, 3, 198 };
-            int pivot = arreglo.Length % 2 > 0 ? arreglo.Length / 2 + 1 : arreglo.Length / 2;
+            int[] arreglo = { 4, 8, -3, 10, -7, -9, 2, -5, 6, 1 };
+            rn = new Random();
 
-            QuickSort(ref arreglo, pivot, arreglo.Length - 1);
-            foreach (int i in arreglo) 
+            opc = int.Parse(Console.ReadLine());
+
+            QuickSort(ref arreglo, 0, arreglo.Length - 1);
+            foreach (int i in arreglo)
             {
                 Console.WriteLine(i);
             }
@@ -21,43 +28,65 @@
             y = temp;
         }
 
-        public static int Partition(ref int[] arreglo, int aleatorio, int longitud)
+        public static int Partition(ref int[] arreglo, int menor, int mayor)
         {
-            int pivot = arreglo[aleatorio];
-            int izq = 0;
-            int der = longitud;
+            int pivot_index;
+            switch (opc)
+            {
+                case 1:
+                    pivot_index = menor;
+                    break;
+
+                case 2:
+                    pivot_index = mayor % 2 > 0 ? Math.Abs(mayor / 2 + 1) : mayor / 2;
+                    break;
+
+                case 3:
+                    pivot_index = mayor;
+                    break;
+
+                default:
+                    pivot_index = rn.Next(menor, mayor);
+                    break;
+            }
+
+            Swap(ref arreglo[menor], ref arreglo[pivot_index]);
+
+            int pivot = arreglo[menor];
+            int izq = menor;
+            int der = mayor;
 
             while (true)
             {
                 while (izq <= der && arreglo[izq] <= pivot)
                 {
-                    izq++;
+                    izq += 1;
                 }
 
                 while (izq <= der && arreglo[der] >= pivot)
                 {
-                    der--;
+                    der -= 1;
                 }
 
-                if (der < izq)
+                if (izq > der)
                 {
                     break;
                 }
 
                 Swap(ref arreglo[izq], ref arreglo[der]);
             }
-            Swap(ref arreglo[aleatorio], ref arreglo[der]);
+            Swap(ref arreglo[menor], ref arreglo[der]);
             return der;
         }
 
-        public static void QuickSort(ref int[] arreglo, int aleatorio, int longitud)
+        public static void QuickSort(ref int[] arreglo, int menor, int mayor)
         {
-            if (aleatorio < longitud)
+            if (menor < mayor)
             {
-                int pivot = Partition(ref arreglo, aleatorio, longitud);
+                int pivot = Partition(ref arreglo, menor, mayor);
 
-                QuickSort(ref arreglo, aleatorio, pivot - 1);
-                QuickSort(ref arreglo, pivot + 1, longitud);
+                QuickSort(ref arreglo, menor, pivot - 1);
+                QuickSort(ref arreglo, pivot + 1, mayor);
             }
         }
     }
